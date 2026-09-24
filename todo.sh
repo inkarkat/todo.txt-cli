008b9f3501ef3e27cd617dfdeaecb094f3cf794c
Ingo Karkat <swdev@ingo-karkat.de>
2026-09-24 12:43:40 +0200

    Refactoring: Minor: Tighten TODOTXT_SIGIL_VALID_PATTERN to only include non-whitespace characters
    The greedy .* doesn't do any harm in the implementation, because it only operates on already extracted (via grep -o) single word-with-sigil candidates.
    But if add-ons want to reuse the pattern for other todo manipulation, the default value prevents that.

--- todo.sh
+++ todo.sh
@@ -172,7 +172,8 @@ $indentedJoinedConfigFileLocations
 		    TODOTXT_FINAL_FILTER="sed ..."  customize list after color, P@+ hiding
 		    TODOTXT_SOURCEVAR=\$DONE_FILE   use another source for listcon, listproj
 		    TODOTXT_SIGIL_BEFORE_PATTERN="" optionally allow chars preceding +p / @c
-		    TODOTXT_SIGIL_VALID_PATTERN=.*  tweak the allowed chars for +p and @c
+		    TODOTXT_SIGIL_VALID_PATTERN='[^ ]\{1,\}'
+		                                    tweak the allowed chars for +p and @c
 		    TODOTXT_SIGIL_AFTER_PATTERN=""  optionally allow chars after +p / @c
 
 	EndVerboseHelp
@@ -669,7 +670,7 @@ TODOTXT_DISABLE_FILTER=${TODOTXT_DISABLE_FILTER:-}
 TODOTXT_FINAL_FILTER=${TODOTXT_FINAL_FILTER:-cat}
 TODOTXT_GLOBAL_CFG_FILE=${TODOTXT_GLOBAL_CFG_FILE:-/etc/todo/config}
 TODOTXT_SIGIL_BEFORE_PATTERN=${TODOTXT_SIGIL_BEFORE_PATTERN:-}    # Allow any other non-whitespace entity before +project and @context; should be an optional match; example: \(w:\)\{0,1\} to allow w:@context.
-TODOTXT_SIGIL_VALID_PATTERN=${TODOTXT_SIGIL_VALID_PATTERN:-.*}    # Limit the valid characters (from the default any non-whitespace sequence) for +project and @context; example: [a-zA-Z]\{3,\} to only allow alphabetic ones that are at least three characters long.
+TODOTXT_SIGIL_VALID_PATTERN=${TODOTXT_SIGIL_VALID_PATTERN:-[^ ]\\{1,\\\}}    # Limit the valid characters (from the default any non-whitespace sequence) for +project and @context; example: [a-zA-Z]\{3,\} to only allow alphabetic ones that are at least three characters long.
 TODOTXT_SIGIL_AFTER_PATTERN=${TODOTXT_SIGIL_AFTER_PATTERN:-}    # Allow any other non-whitespace entity after +project and @context; should be an optional match; example: )\{0,1\} to allow (with the corresponding TODOTXT_SIGIL_BEFORE_PATTERN) enclosing in parentheses.
 
 # Export all TODOTXT_* variables
